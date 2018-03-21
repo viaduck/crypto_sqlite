4a69ba48ee7e34cdb00e898526aab7a3e5528bc5
Steffen Klee <steffen.klee@gmail.com>
2018-03-22 00:18:24 +0100

    Added licenses

--- src/codec/codec.c
+++ src/codec/codec.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2017-2018 The ViaDuck Project
+ *
+ * This file is part of cryptoSQLite.
+ *
+ * cryptoSQLite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cryptoSQLite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with cryptoSQLite.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <sqlite3.c>
 #include <stdint.h>
 #include <stdio.h>
